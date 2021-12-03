39e0342eeefa464447fb48a2e72a0b0c2662cf12
zwelidumsani <zwelidumsani@gmail.com>
2021-12-03 11:57:41 +0200

    latest

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -92,16 +92,16 @@ $(document).ready(function () {
             var curval1 = parseInt($('#counter1').text().replace(' ', ''));
             var curval2 = parseInt($('#counter2').text());
             var curval3 = parseInt($('#counter3').text());
-            if (curval <= 1007) {
+            if (curval <= 0) {
                 $('#counter').text(curval + 1);
             }
-            if (curval1 <= 1280) {
+            if (curval1 <= 0) {
                 $('#counter1').text(sdf_FTS((curval1 + 20), 0, ' '));
             }
-            if (curval2 <= 145) {
+            if (curval2 <= 0) {
                 $('#counter2').text(curval2 + 1);
             }
-            if (curval3 <= 1022) {
+            if (curval3 <= 0) {
                 $('#counter3').text(curval3 + 1);
             }
         }, 2);
